a820a39fe3841c268f7962450bac7cf4bb947657
duckplug <okolelovant@gmail.com>
2021-03-30 18:42:13 +0300

    4.3.5. Множественное логическое выражение И [#491397]

--- test/ru/job4j/condition/TriangleTest.java
+++ test/ru/job4j/condition/TriangleTest.java
@@ -14,4 +14,49 @@ public class TriangleTest {
             boolean result = Triangle.exist(ab, ac, bc);
             Assert.assertTrue(result);
         }
+
+    @Test
+    public void whenExist0 () {
+        double ab = 0;
+        double ac = 2.0;
+        double bc = 2.0;
+        boolean result = Triangle.exist(ab, ac, bc);
+        Assert.assertTrue(result);
     }
+
+    @Test
+    public void whenExist1 () {
+        double ab = 2.0;
+        double ac = 0;
+        double bc = 2.0;
+        boolean result = Triangle.exist(ab, ac, bc);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void whenExist2 () {
+        double ab = 0;
+        double ac = 2.0;
+        double bc = 2.0;
+        boolean result = Triangle.exist(ab, ac, bc);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void whenExist3 () {
+        double ab = 0;
+        double ac = 2.0;
+        double bc = 0;
+        boolean result = Triangle.exist(ab, ac, bc);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void whenExist4 () {
+        double ab = 0;
+        double ac = 0;
+        double bc = 0;
+        boolean result = Triangle.exist(ab, ac, bc);
+        Assert.assertTrue(result);
+    }
+}
